206c2a5e6888589563f45fca8cf170498e5b03c9
Forthtilliath <forth.developer@gmail.com>
2023-05-31 00:24:11 +0200

    try netlify build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import solid from "solid-start/vite";
 import { defineConfig } from "vite";
+import netlify from "solid-start-netlify";
 
 export default defineConfig({
-  plugins: [solid()],
   ssr: { external: ["@prisma/client"] },
+  plugins: [
+    solid({
+      adapter: netlify({ edge: true }),
+    }),
+  ],
 });
